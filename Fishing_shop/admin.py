d2862451de0a53f0c6463f8d76e21bdedd50ecf8
Shved Daniil <danikshved31@.com>
2026-03-10 14:08:26 +0300

    nashel

--- Fishing_shop/admin.py
+++ Fishing_shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Categories, Manufacturer, Product
 
-# Register your models here.
+admin.site.register(Categories)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
--- Fishing_shop/models.py
+++ Fishing_shop/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+def validate_not_negative(value):
+    if value < 0:
+        raise ValueError(
+            ('число не может быть отрицательным')
+        )
+
+class Categories(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    discription = models.TextField(blank=True, verbose_name='Описание')
+    class Meta:
+        verbose_name_plural = 'Категория товара'
+
+def __str__(self):
+    return self.title
+
+class Manufacturer(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    country = models.CharField(max_length=100, verbose_name='Страна')
+    discription = models.TextField(blank=True, verbose_name='Описание')
+    class Meta:
+        verbose_name_plural = 'Производитель'
+
+def __str__(self):
+    return self.title
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    discription = models.TextField(verbose_name='Описание')
+    image = models.ImageField(verbose_name='Фотография')
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[validate_not_negative], verbose_name='Цена')
+    in_stock = models.IntegerField(validators=[validate_not_negative], verbose_name='На складе')
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name='Категория')
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, verbose_name='Производитель')
+    class Meta:
+        verbose_name_plural = 'Продукт'
+
+def __str__(self):
+    return self.title  
+    
+
