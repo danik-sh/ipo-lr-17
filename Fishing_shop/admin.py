de4edddeef3c3db8ea6e418d91ac9aceb5d74bd5
danchik <danikshved31@gmail.com>
2026-03-19 11:26:35 +0300

    end

--- Fishing_shop/admin.py
+++ Fishing_shop/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Categories, Manufacturer, Product
+from .models import Categories, Manufacturer, Product, Bucket, Item_in_bucket
 
 admin.site.register(Categories)
 admin.site.register(Manufacturer)
 admin.site.register(Product)
+admin.site.register(Bucket)
+admin.site.register(Item_in_bucket)
--- Fishing_shop/models.py
+++ Fishing_shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 def validate_not_negative(value):
     if value < 0:
@@ -6,14 +7,16 @@ def validate_not_negative(value):
             ('число не может быть отрицательным')
         )
 
+
+
 class Categories(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
     discription = models.TextField(blank=True, verbose_name='Описание')
     class Meta:
         verbose_name_plural = 'Категория товара'
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 class Manufacturer(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
@@ -22,8 +25,8 @@ class Manufacturer(models.Model):
     class Meta:
         verbose_name_plural = 'Производитель'
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 class Product(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
@@ -36,7 +39,65 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Продукт'
 
-def __str__(self):
-    return self.title  
-    
+    def __str__(self):
+        return self.title  
+        
+class Bucket(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='bucket',
+        verbose_name='пользоваетель'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'   
+    )
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+        ordering = ['-created_at']
 
+    def __str__(self):
+        return  f'Корзина пользователя {self.user.username}'
+    
+    def total_cost(self):
+        ...
+        return 0
+    
+def validator_for_bucket(self):
+     if self.quantity > self.product.stock_quantity:
+            raise ValueError({
+                'quantity': f'Недостаточно товара на складе. Доступно: {self.product.stock_quantity} шт.'
+            })
+     
+class Item_in_bucket(models.Model):
+    bucket = models.ForeignKey(
+        Bucket,
+        on_delete=models.CASCADE,
+        related_name='items',
+        verbose_name='Корзины'
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='bucket_items',
+        verbose_name='Товар'
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name='Количество',
+        validators=[validator_for_bucket]
+    )
+
+    def __str__(self):
+        return f'{self.product.title} ({self.quantity} шт.)'
+    
+    def item_price(self):
+        return self.product.price * self.quantity
+    
+    class Meta:
+        verbose_name_plural = 'Объект корзины'
+    
